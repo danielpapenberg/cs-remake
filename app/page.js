8379b34c71a57bf800670d9c4ad97e0f1f76d09a
Daniel Papenberg <daniel.papenberg@arvato-scs.com>
2023-12-06 17:53:02 +0100

    test

--- app/page.js
+++ app/page.js
@@ -1,113 +1,70 @@
-import Image from 'next/image'
+import Bubble from './components/bubble.client'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTwitter, faTelegram } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <main>
+            <div className="bg"></div>
+            <div className='h-[100vh]'></div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <Bubble />
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <div className='absolute xl:right-[20%] top-[20%] xl:top-[40%] p-5 xl:p-0'>
+                <h2 className='text-[60px] xl:text-[120px] leading-none uppercase'>Empower</h2>
+                <h2 className='text-[60px] xl:text-[120px] leading-none text-[#6A90BA] uppercase font-bold overflow-hidden'>
+                    Your&nbsp;
+                    <span className="word-slider">
+                        <span className="slide">Crypto</span>
+                        <span className="slide">Life</span>
+                    </span>
+                </h2>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                <p className='text-[18px] m-0 p-0 uppercase w-full xl:w-[840px] text-left mt-4'>
+                    Crypto Society is a group of enthusiasts set up by the people for the people.
+                    We aim to inform on all matters crypto by sharing our research to passionate
+                    members who are encouraged to learn and share themselves.
+                </p>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <div className='social relative xl:h-[80vh] p-5 xl:p-0'>
+                <div className='xl:absolute xl:right-[20%] top-[10%] w-full xl:w-[1200px]'>
+                    <h2 className='text-[60px] xl:text-[120px] m-0 p-0 leading-none uppercase'><span className='text-[#6A90BA] uppercase font-bold'>CONNECT</span> WITH US</h2>
+                    <div className='flex flex-col xl:flex-row gap-5 xl:gap-20 xl:items-end '>
+                        <div className='flex items-center gap-10 mt-12'>
+                            <FontAwesomeIcon icon={faTelegram} className='h-[50px] xl:h-[100px]' />
+                            <span className='text-[18px] uppercase'>Telegram</span>
+                        </div>
+                        <div className='flex items-center gap-10 mt-5'>
+                            <FontAwesomeIcon icon={faTwitter} className='h-[50px] xl:h-[100px]' />
+                            <span className='text-[18px] uppercase'>Twitter</span>
+                        </div>
+                    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+                    <div className='flex flex-col xl:flex-row gap-10 xl:gap-20 mt-24'>
+                        <div>
+                            <h2 className='text-[40px] xl:text-[100px] m-0 p-0 leading-none uppercase text-[#6A90BA] font-bold'>LEARN</h2>
+                            <p>We bring awareness of the latest ground-breaking crypto projects to our community, including hosting regular AMA’s and live TA Market Overviews, thus teaching how to research and apply trading and hodling strategies.</p>
+                        </div>
+                        <div>
+                            <h2 className='text-[40px] xl:text-[100px] m-0 p-0 leading-none uppercase text-[#6A90BA] font-bold'>APPLY</h2>
+                            <p>With this applied learning the group is encouraged to research and share their findings with our members, actively getting involved, effectively benefitting the entire community.</p>
+                        </div>
+                        <div>
+                            <h2 className='text-[40px] xl:text-[100px] m-0 p-0 leading-none uppercase text-[#6A90BA] font-bold'>SUCCEED</h2>
+                            <p>Crypto is a bumpy road but with so many eyes and ears actively contributing we regularly (and) successfully recognize great buying opportunities that ultimately lead to a lucrative investment.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='hidden xl:block xl:text-[1200px] absolute z-[-1] right-[50px] top-[-500px] text-[#212121] font-serif'>&</div>
+            </div>
+
+            <div className='partner p-5'>
+                <h2 className='text-[60px] xl:text-[120px] m-0 p-0 leading-none uppercase'>
+                    OUR<p className='text-[#6A90BA] uppercase font-bold'>Partner</p></h2>
+            </div>
+            <div className='h-[5000px]'></div>
+            
+        </main>
+    )
 }
