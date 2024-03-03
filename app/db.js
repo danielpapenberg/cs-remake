55e4f5b8697f0cba00eb10683114a5c740c502b0
Daniel Papenberg <daniel.papenberg@arvato-scs.com>
2024-03-03 12:57:41 +0100

    db test

--- app/db.js
+++ app/db.js
@@ -10,7 +10,7 @@ const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: databaseName,
+    database: process.env.MYSQL_DATABASE,
     port: process.env.MYSQL_PORT,
     waitForConnections: true,
     connectionLimit: 50,
